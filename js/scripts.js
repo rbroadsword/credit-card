4d8a7ba4b2e40351eface9c35f9282da112332d7
Ryan Broadsword <rbroadsword@gmail.com>
2022-04-20 20:10:33 -0700

    final solution

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,54 @@
 function creditCard(number) {
+  let result = 0;
   let userInput = number.split("");  
   const toNumbers = userInput.map(function(number){ //change userInput to array of #
     return parseInt(number);
   });
 
   const doubleArray = [] //everyother # from right to left
-  console.log(toNumbers);
   for (let i = toNumbers.length - 1; i >= 0; i-=1) { //getting everyother number r to L
     if(toNumbers[i] % 2 === 0) {
-          doubleArray.push(toNumbers[i]*2);
+          doubledNumber = toNumbers[i]*2;
+    }else if(doubledNumber > 9){
+            doubledNumber = doubledNumber.toString().split("");
+            doubledNumber = doubledNumber.map(Number);
+            sumOfTwo = doubledNumber[0] + doubledNumber[1];
+            doubleArray.push(sumOfTwo);
     } else{
       doubleArray.push(toNumbers.at(i));
     }
-    console.log(doubleArray);
-    console.log(toNumbers); 
+  }
+  result = doubleArray.reduce(function summarize(sum, number, index) {
+      return sum + number; 
+    });
+  if(result.length -1 === 0) {
+    console.log("valid");
+  } else {
+    console.log("not valid");
   }
 }
 
-
-for (let i = toNumbers.length - 1; i >= 0; i-=1) { //getting everyother number r to L
-  if(toNumbers[i] % 2 === 0) {
-    doubledNumber = toNumbers[i]*2;
-    console.log(doubledNumber);
-    if(doubledNumber > 9){
-      doubledNumber = doubledNumber.split(""); 
-      sumOfTwo = doubledNumber.reduce(doubledNumber);
-      console.log(sumOfTwo); 
-      doubleArray.push(sumOfTwo);
+function double(number) {
+  let testInput = number.split(""); 
+  const toNumbers = testInput.map(function(number){ //change userInput to array of #
+    return parseInt(number);
+  });
+  for (let i = toNumbers.length - 1; i >= 0; i-=1) { //getting everyother number r to L
+    if(toNumbers[i] % 2 === 0) {
+      doubledNumber = toNumbers[i]*2;
+      if(doubledNumber > 9){
+        doubledNumber = doubledNumber.toString().split("");
+        console.log(doubledNumber);
+        doubledNumber = doubledNumber.map(Number);
+        console.log(doubledNumber);
+        sumOfTwo = doubledNumber[0] + doubledNumber[1];
+        console.log(sumOfTwo); 
+        doubleArray.push(sumOfTwo);
+      } else {
+        console.log("nope"); 
+      }
     }
-    
-  } else{
-    doubleArray.push(toNumbers.at(i));
   }
-  console.log(doubleArray);
-  console.log(toNumbers); 
 }
 
 
@@ -62,11 +77,32 @@ function validate(n) {
   return sum % 10 === 0; 
 }
 
-const doubleArray = [] //everyother # from right to left
-console.log(toNumbers);
-for (let i = toNumbers.length - 1; i >= 0; i-=1) { //getting everyother number r to L
-  if(toNumbers[i] % 2 === 0) {
-    doubleArray.push(toNumbers[i]*2);
-  } else{
-    doubleArray.push(toNumbers.at(i));
+//Example Two
+function validate(n) {
+  n = n.toString().split(" ").map (Number).reverse(); 
+  return n.reduce(function (sum, digit, index) {
+    if (index & 1) digit <<= 1; 
+    if (digit > 9) digit -= 9; 
+    return sum + digit; 
+  }, 0) % 10 == 0; 
+}
+
+//Example Three 
+
+function validate(n){
+  let sum = 0;
+
+  while (n > 0) {
+    let a = n % 10;
+    n = Math.floor(n / 10);
+
+    let b = (n % 10) * 2;
+    n = Math.floor(n / 10);
+
+    if (b > 9) {
+      b -= 9;
+    }
+    sum += a + b;
+  }
+  return sum % 10 == 0;
 }
